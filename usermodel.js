0ae99e32df6d0a73e3782435ee444df5a9ee675f
Nisha2025-droid <nisha1styear@gmail.com>
2026-01-17 17:51:01 +0530

    Add files via upload

---
+++ usermodel.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/chavi'); 
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,  
+    username: String, 
+});
+module.exports = mongoose.model('User', userSchema);
